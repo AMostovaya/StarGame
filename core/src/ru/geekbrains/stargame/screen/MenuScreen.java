54cdc0dfddce0990f33ab539f43d18962dca61f6
AMostovaya <amostovaya86@gmail.com>
2019-11-14 00:30:16 +0600

    // добавлен вызов класса Ship, и его события touchdown и keydown

--- core/src/ru/geekbrains/stargame/screen/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -1,33 +1,50 @@
 package ru.geekbrains.stargame.screen;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.stargame.base.BaseScreen;
 import ru.geekbrains.stargame.math.Rect;
 import ru.geekbrains.stargame.sprite.Background;
-import ru.geekbrains.stargame.sprite.Logo;
+import ru.geekbrains.stargame.sprite.ButtonExit;
+import ru.geekbrains.stargame.sprite.ButtonPlay;
+import ru.geekbrains.stargame.sprite.Star;
 
 public class MenuScreen extends BaseScreen {
 
-    private Texture img;
+    private static final int STAR_COUTNT = 256;
     private Texture bg;
     private Background background;
-    private Logo logo;
+    private Game game;
+    private TextureAtlas atlas;
+    private Star[] stars;
+    private ButtonExit buttonExit;
+    private ButtonPlay buttonPlay;
+
+    public MenuScreen(Game game) {
+        this.game = game;
+    }
 
     @Override
     public void show() {
 
         super.show();
-
-        img = new Texture("ScreenSaver.png");
         bg = new Texture("textures/bg.png");
-        logo = new Logo(new TextureRegion(img));
-        logo.setHeightProportion(0.5f);
         background = new Background(new TextureRegion(bg));
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        stars = new Star[STAR_COUTNT];
+        for (int i =0; i< STAR_COUTNT; i++) {
+            stars[i] = new Star(atlas);
+
+        }
+        buttonExit = new ButtonExit(atlas);
+        buttonPlay = new ButtonPlay(atlas, game);
+
     }
 
     @Override
@@ -66,14 +83,17 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public void dispose() {
-        img.dispose();
+
         bg.dispose();
+        atlas.dispose();
+
         super.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer) {
-        logo.touchDown(touch, pointer);
+        buttonExit.touchDown(touch, pointer);
+        buttonPlay.touchDown(touch, pointer);
         return false;
     }
 
@@ -83,14 +103,31 @@ public class MenuScreen extends BaseScreen {
     }
 
     @Override
+    public boolean touchUp(Vector2 touch, int pointer) {
+
+        buttonExit.touchUp(touch, pointer);
+        buttonPlay.touchUp(touch, pointer);
+        return false;
+
+    }
+
+    @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        for (Star star: stars) {
+            star.resize(worldBounds);
+        }
+        buttonExit.resize(worldBounds);
+        buttonPlay.resize(worldBounds);
     }
 
     private void update(float delta) {
 
-        logo.update(delta);
+        for (Star star: stars) {
+            star.update(delta);
+        }
+
     }
 
     private void draw() {
@@ -98,7 +135,12 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         background.draw(batch);
-        logo.draw(batch);
+        for (Star star: stars) {
+            star.draw(batch);
+        }
+
+        buttonExit.draw(batch);
+        buttonPlay.draw(batch);
         batch.end();
 
     }
