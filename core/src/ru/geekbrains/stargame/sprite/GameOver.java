1801782951ca8f9720f34c970f77a3ea924113ec
AMostovaya <amostovaya86@gmail.com>
2019-11-25 00:05:45 +0600

    // добавлен новый класс, для вывода надписи "Game over"

---
+++ core/src/ru/geekbrains/stargame/sprite/GameOver.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.stargame.sprite;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import ru.geekbrains.stargame.base.Sprite;
+
+public class GameOver extends Sprite {
+
+    public GameOver(TextureAtlas atlas) {
+       super(atlas.findRegion("message_game_over"));
+       setHeightProportion(0.05f);
+       setBottom(0.01f);
+
+    }
+}
