6d5f848df472fb22e1e3844e0ce9e36bf15d36df
AMostovaya <amostovaya86@gmail.com>
2019-11-25 00:37:45 +0600

    // добавлен новый класс вывода надписи окончания игры

--- core/src/ru/geekbrains/stargame/sprite/GameOver.java
+++ core/src/ru/geekbrains/stargame/sprite/GameOver.java
@@ -6,6 +6,7 @@ import ru.geekbrains.stargame.base.Sprite;
 public class GameOver extends Sprite {
 
     public GameOver(TextureAtlas atlas) {
+
        super(atlas.findRegion("message_game_over"));
        setHeightProportion(0.05f);
        setBottom(0.01f);
